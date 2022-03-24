f88eb141056ab3dae4a78f1c04b3878859697822
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-24 17:33:29 +0530

    Update jenkins_cd.groovy

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -3,7 +3,7 @@ def hostname() {
 }
 
 def sftp_get(String credential_store_name) {
-  withCredentials([sshUserPrivateKey(credentialsId:${credential_store_name}, keyFileVariable: 'keyfile',usernameVariable: 'USERNAME')]) {
+  withCredentials([sshUserPrivateKey(credentialsId:"${credential_store_name}", keyFileVariable: 'keyfile',usernameVariable: 'USERNAME')]) {
     sh "echo -oStrictHostKeyChecking=no -i ${keyfile} ${USERNAME}"
   }
 }
