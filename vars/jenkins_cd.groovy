cae8f92c48719c95eee083b5d9426be73af9c5db
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-24 17:32:31 +0530

    fixed String format

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -2,7 +2,7 @@ def hostname() {
   sh '''hostname -f'''
 }
 
-def sftp_get(credential_store_name) {
+def sftp_get(String credential_store_name) {
   withCredentials([sshUserPrivateKey(credentialsId:${credential_store_name}, keyFileVariable: 'keyfile',usernameVariable: 'USERNAME')]) {
     sh "echo -oStrictHostKeyChecking=no -i ${keyfile} ${USERNAME}"
   }
