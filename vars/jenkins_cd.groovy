cf277ddfc961aa9267f6f921791b9b699885c2d1
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-28 22:42:59 +0530

    Update jenkins_cd.groovy

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -13,18 +13,10 @@ def push_github(Map github_args = [:])
   withCredentials([usernamePassword(credentialsId: "${github_args.credential_github_name}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
     dir("${WORKSPACE}/${github_args.target}")
     {
-      sh '''git clone https://github.com/nafasat/testing_git.git;
-      echo "${github_args.tar_archive_name}" > out.txt;
-      unzip "${github_args.tar_archive_name}";
-      rm -rf "${github_args.tar_archive_name}";
-      cp ./ansible.cfg ./testing_git/;
-      cd ./testing_git/;
-      git checkout dtesting;
-      git add -A;
-      git config --global user.email "${github_args.jenkins_user_email}";
-      git config --global user.name "${github_args.jenkins_user}";
-      git commit -m "${commit_msg}";
-      git push https://"${USERNAME}":"${PASSWORD}"@github.com/nafasat/testing_git.git'''
+      sh '''
+      git clone https://github.com/nafasat/testing_git.git;
+      echo "Hello ${github_args.tar_archive_name}" > out.txt;
+      '''
     }
   }
 }
