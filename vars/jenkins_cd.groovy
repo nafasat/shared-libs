f0da464408ac92a9f7a33bdbd5e05dbf30907b32
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-28 22:47:22 +0530

    Update jenkins_cd.groovy

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -15,7 +15,7 @@ def push_github(Map github_args = [:])
     {
       sh '''
       git clone https://github.com/nafasat/testing_git.git
-      unzip "${github_args.tar_archive_name}";
+      echo "${github_args.tar_archive_name}"
       '''
     }
   }
