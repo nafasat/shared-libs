635a9ecb8a2b73a38764cb0397124c6a304f4141
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-30 12:36:40 +0530

    remove debug

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -7,7 +7,6 @@ def sftp_get(Map sftp_args = [:]) {
     dir("${WORKSPACE}/")
     {
       sh "echo get ${sftp_args.target_path}/${sftp_args.tar_archive_name} | sshpass -p ${PASSWORD} sftp -q -oStrictHostKeyChecking=no ${USERNAME}@${sftp_args.sftp_ip}"
-      sh "echo sftp_get $PWD"
     }
   }
 }
@@ -27,8 +26,6 @@ def push_github_script(Map config = [:]) {
   withCredentials([usernamePassword(credentialsId: "${config.credential_github_name}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
     loadGitHubScript(name: 'push_github.sh')
     sh "./push_github.sh ${USERNAME} ${PASSWORD} ${config.commit_msg} ${config.archive_name} ${config.repo_name_without_https}"
-    sh "echo push_github_script $PWD"
-    sh "ls -l"
   }
 }
 
