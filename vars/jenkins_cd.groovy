0479b5cd2a15c6b6fecdfff0178cadf2927f6ebb
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-30 16:43:23 +0530

    Update jenkins_cd.groovy

--- vars/jenkins_cd.groovy
+++ vars/jenkins_cd.groovy
@@ -6,11 +6,7 @@ def sftp_get(Map sftp_args = [:]) {
   withCredentials([usernamePassword(credentialsId: "${sftp_args.credential_sftp_name}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
     dir("${WORKSPACE}/")
     {
-      if (binding.hasVariable("${sftp_args.sftp_ip}") && "${sftp_args.sftp_ip}"?.trim()) {
         sh "echo get ${sftp_args.target_path}/${sftp_args.tar_archive_name} | sshpass -p ${PASSWORD} sftp -q -oStrictHostKeyChecking=no ${USERNAME}@${sftp_args.sftp_ip}"
-      } else
-        println("SFTP IP is blank/null")
-        error("Aborting the build.")
     }
   }
 }
