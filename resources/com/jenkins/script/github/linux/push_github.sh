0fd0c849d449b063a3973eb07d05dcf35c2f6096
nafasat <41534389+nafasat@users.noreply.github.com>
2022-03-30 11:04:41 +0530

    Update push_github.sh

--- resources/com/jenkins/script/github/linux/push_github.sh
+++ resources/com/jenkins/script/github/linux/push_github.sh
@@ -1,10 +1,8 @@
 USERNAME="$1"
 PASSWORD="$2"
 commit_msg="$3"
-jenkins_user_email="$4"
-jenkins_user="$5"
-archive_name="$6"
-repo_name_without_https="$7"
+archive_name="$4"
+repo_name_without_https="$5"
 git clone https://"${USERNAME}":"${PASSWORD}"@"${repo_name_without_https}"
 if [ -f ${archive_name} ]
 then
@@ -16,13 +14,11 @@ fi
 main_file_name=`unzip -Z1 "${archive_name}"`
 unzip -o "${archive_name}"
 rm -rf "${archive_name}"
-repo_name_only=$(basename "$7" .git)
+repo_name_only=$(basename "$repo_name_without_https" .git)
 alias cp='cp'
 cp "${main_file_name}" ./"${repo_name_only}"/
 cd ./"${repo_name_only}"
 git checkout dtesting
 git add -A
-git config --global user.email "${jenkins_user_email}"
-git config --global user.name "${jenkins_user}"
 git commit -m "${commit_msg}"
 git push https://"${USERNAME}":"${PASSWORD}"@"${repo_name_without_https}"
