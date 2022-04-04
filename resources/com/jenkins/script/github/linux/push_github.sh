c39a396b385c89de066f45a1c965e9e68b5decb0
nafasat <41534389+nafasat@users.noreply.github.com>
2022-04-04 12:43:04 +0530

    Update push_github.sh

--- resources/com/jenkins/script/github/linux/push_github.sh
+++ resources/com/jenkins/script/github/linux/push_github.sh
@@ -6,25 +6,24 @@ archive_name="$4"
 repo_name_without_https="$5"
 git clone https://"${USERNAME}":"${PASSWORD}"@"${repo_name_without_https}"
 repo_name_only=$(basename "$repo_name_without_https" .git)
+alias cp='cp'
 if [ -f ${archive_name} ]
 then
-  unzip -o "${archive_name}"
+  if [[ `unzip -Zl "${archive_name}" | grep 'unx' | cut -f1 -d' '` =~ d+ ]]
+  then
+    main_file_name=`unzip -Z1 $archive_name | head -1 | sed 's/\/$//g'`
+    unzip -o "${archive_name}" -d tmp_"${main_file_name}"
+    rm -rf "${archive_name}"
+    cp -r tmp_"${main_file_name}"/"${main_file_name}"/* ./"${repo_name_only}"/
+  else
+    main_file_name=`unzip -Z1 "${archive_name}"`
+    unzip -o "${archive_name}"
+    rm -rf "${archive_name}"
+    cp "${main_file_name}" ./"${repo_name_only}"/
+  fi
 else
-  echo "File doesn't exist ${archive_name}"
+    echo "File doesn't exist ${archive_name}"
   exit 1
-fi
-main_file_name=`unzip -Z1 "${archive_name}"`
-alias cp='cp'
-if [[ `unzip -Zl "${archive_name}" | grep 'unx' | cut -f1 -d' '` =~ d+ ]]
-then
-  main_file_name=`unzip -Z1 $archive_name | head -1 | sed 's/\/$//g'`
-  unzip -o "${archive_name}" -d tmp_"${main_file_name}"
-  rm -rf "${archive_name}"
-  cp -r tmp_"${main_file_name}"/"${main_file_name}"/* ./"${repo_name_only}"/
-else
-  unzip -o "${archive_name}"
-  rm -rf "${archive_name}"
-  cp "${main_file_name}" ./"${repo_name_only}"/
 fi
 cd ./"${repo_name_only}"
 git checkout dtesting
