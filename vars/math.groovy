c72c4f9c2c3dd227c005b9bbaca8083ab1e5dc21
nafasat <41534389+nafasat@users.noreply.github.com>
2022-04-08 14:46:45 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -16,3 +16,13 @@ def test_if_condition(Map config=[:]) {
     sh "echo ${config.name}"
   }
 }
+
+def git_clone(Map config=[:]) {
+  checkout(
+    [$class: 'GitSCM',
+    branches: [[name: '*/master']], 
+    doGenerateSubmoduleConfigurations: false, 
+    extensions: [], 
+    submoduleCfg: [], 
+     userRemoteConfigs: [[credentialsId: "$config.credential_github_name}", url: 'https://github.com/nafasat/testing_git.git']]])
+}
