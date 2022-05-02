65bc3924b720f0279ab8c5ad836d460e97a5ee68
nafasat <41534389+nafasat@users.noreply.github.com>
2022-05-02 12:39:46 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -5,7 +5,7 @@ def check_val(check_conf=[:]) {
   }
 }
 def custom_fun(Map config=[:]) {
-  if (config.containKey('key')) {
+  if (config.containsKey('key')) {
     println("echo ssh with key ${custom_fun.key}")
   } else {
     println("echo ssh without key !")
